c31a03317bb48d1a381ec8ebd1b6a3eec8cd750e
Narasimmamoorthy Sukumar <163540678+N-Moorthy@users.noreply.github.com>
2024-08-12 19:25:13 +0530

    index.js

--- index.js
+++ index.js
@@ -13,24 +13,16 @@ clear();
 var prompt = inquirer.createPromptModule();
 
 const data = {
-  name: chalk.bold.blue("Aditya Chamim Pratama"),
+  name: chalk.bold.blue("Narasimmamoorthy Sukumar"),
   nickname: chalk.bold("Adityacprtm"),
 
-  web: chalk.green("https://adityacprtm.dev    "),
-  blog: chalk.green("https://adityacprtm.dev/blog "),
-  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("adityacprtm"),
-  npm: chalk.gray("https://npmjs.com/") + chalk.red("~adityacprtm"),
-  github: chalk.gray("https://github.com/") + chalk.white("Adityacprtm"),
-  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("adityacprtm"),
-  npx: chalk.red("npx") + " " + chalk.white("adityacprtm"),
+  web: chalk.green("https://nmoorthy.site    "),
+  github: chalk.gray("https://github.com/") + chalk.white("N-Moorthy"),
+  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("narasimmamoorthy-sukumar"),
 
   labelWeb: chalk.white.bold("Web:"),
-  labelBlog: chalk.white.bold(" Blog:"),
-  labelTwitter: chalk.white.bold("Twitter:"),
-  labelNpm: chalk.white.bold("  npm:"),
   labelGitHub: chalk.white.bold("GitHub:"),
   labelLinkedIn: chalk.white.bold("  LinkedIn:"),
-  labelCard: chalk.white.bold("Card:"),
 };
 
 const box = boxen(
@@ -38,13 +30,8 @@ const box = boxen(
     `${data.name} | ${data.nickname} 🚀`,
     ``,
     `${data.labelWeb}  ${data.web}`,
-    `${data.labelBlog}  ${data.blog}`,
-    `${data.labelTwitter}  ${data.twitter}`,
-    `${data.labelNpm}  ${data.npm}`,
     `${data.labelGitHub}  ${data.github}`,
     `${data.labelLinkedIn}  ${data.linkedin}`,
-    ``,
-    `${data.npx}`,
     ``,
     `${chalk.italic.bold("I am an enthusiastic newbie.")}`,
     `${chalk.italic("There is much to learn and much more to build.")}`,
@@ -69,7 +56,7 @@ const questions = [
       {
         name: `Send me an ${chalk.green.bold("email")}?`,
         value: () => {
-          open("mailto:aditya@adityacprtm.dev");
+          open("mailto:nmoorthy.s@yahoo.com");
           console.log(
             "\nPlease wait, your email application will pop up soon,\nsee ya.\n"
           );
